457ff8ccaa27825e64c43d184fbeb5725c9d34be
AccountBurner <ZekeGBlox@protonmail.com>
2025-06-12 16:00:46 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3879,8 +3879,8 @@ function Library:CreateWindow(...)
 	});
 
 	local WindowLabel = Library:CreateLabel({
-		Position = UDim2.new(0, 12, 0, 4); 
-		Size = UDim2.new(0, 0, 0, 25);
+		Position = UDim2.new(0, 20, 0, 4); 
+		Size = UDim2.new(1, -40, 0, 25); 
 		Text = Config.Title or '';
 		TextXAlignment = Enum.TextXAlignment.Left;
 		ZIndex = 1;
